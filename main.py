550def12be4565de377fcfbd7c0bcd3dcc456d8e
Margarita Montenegro <margamontv@gmail.com>
2023-04-04 14:03:31 +0200

    kata_damned_pipelines

--- main.py
+++ main.py
@@ -1,8 +1,72 @@
 #imports
+from modules import module_functions as mf
+import os
+from dotenv import load_dotenv
 
+#Inputs
+load_dotenv('.env')
+MMV_TOKEN = os.environ.get("MMV_TOKEN")
 
-def main():
-  
+API_TOKEN = MMV_TOKEN #API TOKEN (REMEMBER: do not push these to your repo)
+USERNAME = "MargaMontV"
+BASE_URL = 'https://api.github.com/'
+KEY = 'repos/'
+OWNER = 'ih-datapt-mad/'
+REPO = "dataptmad0223_labs/"
+SEARCH = 'search/issues?q=repo:'+OWNER+REPO+'+type:pr+state:{}'
+PULLS = 'pulls?page={}&per_page=100&state={}'
+COMMITS = 'pulls/{}/commits'
+STATE = 'open'
+
+field_list1 = ['number',
+               'title',
+               'state',
+               'created_at',
+               'updated_at',
+               'closed_at',
+               'html_url',
+               'base.repo.full_name',
+               'base.ref',
+               'head.repo.full_name',
+               'head.ref',
+               'head.repo.pushed_at']
+
+field_list2 = ['student_name',
+               'number',
+               'lab_name',
+               'state',
+               'lab_status',
+               'created_at',
+               'updated_at',
+               'closed_at',
+               'html_url',
+               'base.repo.full_name',
+               'base.ref',
+               'head.repo.full_name',
+               'head.ref',
+               'head.repo.pushed_at']
+
+field_sort1 = ['lab_status',
+               'lab_name',
+               'student_name']
+
+field_name1 = ['Student Name',
+               'PR Number',
+               'Lab Name',
+               'PR Status',
+               'Lab Status',
+               'PR Created at',
+               'PR Updated at',
+               'PR Closed at',
+               'PR URL',
+               'base repository',
+               'base',
+               'head repository',
+               'compare',
+               'Pushed at']
+
 
 if __name__ == '__main__':
-    main()
+    DF_PULLS = mf.get_pulls(BASE_URL, KEY, OWNER, REPO, PULLS, SEARCH, STATE, USERNAME, API_TOKEN, field_list1)
+    DF_STATUS = mf.df_status(DF_PULLS, BASE_URL, KEY, OWNER, REPO, COMMITS, USERNAME, API_TOKEN, field_list2)
+    DF_CSV = mf.create_csv(DF_STATUS, field_sort1, field_name1)
---
+++ modules/auxiliary_functions.py
@@ -0,0 +1,69 @@
+#Auxiliary Functions
+
+import requests
+import pandas as pd
+import re
+import json
+import math
+
+def pages(base_url, search, state, username, api_token):
+    pages = requests.get(base_url + search.format(state), auth=(username,api_token)).json()['total_count']
+    if STATE == 'open':
+        pages = math.ceil(pages/100)
+        return pages
+    elif STATE == 'closed':
+        pages = math.ceil(pages/100)
+        return pages
+
+def get_commits(base_url, key, owner, repo, commits, pull, username, api_token):
+    r_commits = requests.get(base_url + key + owner + repo + commits.format(pull),
+                             auth=(username, api_token)).json()
+    df_commits = pd.json_normalize(r_commits)
+    list_commits = list(df_commits['commit.message'])
+    commit = list(set([commit if commit == 'lab-finished' else 'lab-started' for commit in list_commits]))
+    if 'lab-finished' in commit:
+        return 'lab-finished'
+    else:
+        return 'lab-started'
+
+def student_name(x):
+    if ']' in x:
+        x = x.split(']')
+        x = x[1].replace('_', ' ').strip()
+        len_x = len(x.split(' '))
+        if len_x > 1:
+            x = re.findall('\w[a-zA-Z áéíóúÁÉÍÓÚñÑ-]+', x)
+            x = x[0].strip()
+            return x
+        else:
+            x = 'No student name provided'
+            return x
+    else:
+        x = 'Pull request is not properly named'
+        return x
+
+def lab_name(x):
+    if ']' in x:
+        x = x.split(']')
+        x = x[0] + ']'
+        x = x.strip()
+        lower_case = re.findall('[A-ZÁÉÍÓÚñÑ]+', x)
+        if x[0] == '[' and x[-1] == ']' and ' ' not in x and len(lower_case) == 0:
+            return x
+        else:
+            x = 'Lab format name is incorrect'
+            return x
+    else:
+        x = 'Pull request is not properly named'
+        return x
+
+def time_parser(x):
+    try:
+        x = x.strip()
+        x = re.findall('[0-9]+', x)
+        x = ''.join(x)
+        x = pd.to_datetime(x, format='%Y%m%d%H%M%S', errors='coerce')
+        return x
+    except:
+        return 'Nothing pushed yet'
+
